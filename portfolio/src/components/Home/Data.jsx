e5e22046152797c3b409a0e7f4fdac6d01c6601d
Satyam Singh <satyamrajput7239@gmail.com>
2024-09-10 14:43:39 +0530

    home

---
+++ portfolio/src/components/Home/Data.jsx
@@ -0,0 +1,23 @@
+import { IoHandLeft } from "react-icons/io5";
+import { IoIosSend } from "react-icons/io";
+const Data = () => {
+  return (
+    <div className="home__data">
+      <h1 className="home__title">
+        Satyam Singh
+        <IoHandLeft className="home__hand" style={{ color: "orange" }} />
+      </h1>
+      <h3 className="home__subtitle">Software Developer</h3>
+      <p className="home__description">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dicta,
+        voluptatem deserunt. Facilis atque accusamus dolore enim dolores odit
+        quo amet.
+      </p>
+      <a href="#contact" className="button button--flex">
+        {"Say Hello "}
+        <IoIosSend style={{ color: "hsl(0, 0%, 98%)", fontSize: "18px" }} />
+      </a>
+    </div>
+  );
+};
+export default Data;
